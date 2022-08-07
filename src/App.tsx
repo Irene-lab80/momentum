2198d65c5eced532f87941aa60d83e9882c96251
Irina <irene7744@gmail.com>
2022-08-07 14:31:11 +0300

    fix:  prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Momentum</h1>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
+// TODO
+// Часы и календарь
+// Приветствие
+// Смена фонового изображения
+// Виджет погоды
+// Виджет цитата дня
+// Аудиоплеер
+// Перевод
+// Flickr/Unsplash API
+// Настройки en/rus
+// ToDo List
+export default App
