17edd58c085e8b5b767e67cce526718b47e98e1b
Irina <irene7744@gmail.com>
2022-08-27 11:00:41 +0300

    fix: tags and mobile width

--- src/styles.ts
+++ src/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const MainScreen = styled.div`
   height: 100vh;
-  width: 60vh;
+  width: 100vw;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
@@ -21,12 +21,12 @@ export const Header = styled.header`
   margin: 20px;
 `;
 
-export const Main = styled.header`
+export const Main = styled.main`
   height: 40vh;
   margin: 20px;
 `;
 
-export const Footer = styled.header`
+export const Footer = styled.footer`
   height: 30vh;
   margin: 20px;
 `;
