46ab1eca9ddebc580815ea85056577c5c8b53aad
Irina <irene7744@gmail.com>
2022-08-30 08:15:20 +0300

    feat: add data to localstorage forweather component

--- src/components/common/Weather/index.tsx
+++ src/components/common/Weather/index.tsx
@@ -1,46 +1,59 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DisplayWeather from './DisplayWeather';
 
 import { Container, Input } from './styles';
 
 const Weather = () => {
   const APIKEY = '31d9b62edfb8c214c2eabeef6f13b51c';
-  const [form, setForm] = useState({
-    city: ''
-  });
 
-  const [weather, setWeather] = useState({});
-  const [message, setMessage] = useState('');
+  // set query
+  const [city, setCity] = useState(() => {
+    const saved = localStorage.getItem('city') as string;
+    const initialValue = JSON.parse(saved);
+    return initialValue || '';
+  });
+  useEffect(() => {
+    localStorage.setItem('city', JSON.stringify(city));
+  }, [city]);
+
+  // set weather
+  const [weather, setWeather] = useState(() => {
+    const saved = localStorage.getItem('weather') as any;
+    const initialValue = JSON.parse(saved);
+    return initialValue || {};
+  });
+  useEffect(() => {
+    localStorage.setItem('weather', JSON.stringify(weather));
+  }, [weather]);
+
+  // get data from api
   const weatherData = async (e: any) => {
     e.preventDefault();
     let data = {};
     try {
-      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${form.city}&lang=ru&appid=${APIKEY}&units=metric`);
-
-      data = result.data;
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${APIKEY}&units=metric`);
+      data = await result.data;
       setWeather({ data });
-      // console.log(data);
     } catch (error) {
-      setMessage('nothing found');
       console.log(error);
     }
   };
 
+  // handle input
   const handleChange = (e: any) => {
     const { name, value } = e.target;
     if (name === 'city') {
-      setForm({ ...form, city: value });
+      setCity(value);
     }
   };
 
   return (
     <Container>
       <form onSubmit={weatherData}>
-        <Input type="text" name="city" placeholder="City" onChange={(e) => handleChange(e)} />
+        <Input type="text" name="city" value={city} placeholder="City" onChange={(e) => handleChange(e)} />
       </form>
-      {/* @ts-ignore */}
-      {weather.data ? <DisplayWeather data={weather} /> : message}
+      {weather.data ? <DisplayWeather data={weather} /> : null}
     </Container>
   );
 };
